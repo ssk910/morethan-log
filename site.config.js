0bf2c0daea01b5141ac229c80d43bad3ca088e39
sghwang <125856543+sghwang-mcorpor@users.noreply.github.com>
2023-12-01 20:43:09 +0900

    chore: Change utterances label

--- site.config.js
+++ site.config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "💬 Utterances",
+      label: "💬 Comments",
     },
   },
   cusdis: {
